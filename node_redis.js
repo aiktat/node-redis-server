d5133d4e516211ee64506187cb76d3e8a355f0e7
Kien Weng <mycoder80@gmail.com>
2013-07-12 21:35:36 +0800

    updated to support socket.io client

--- node_redis.js
+++ node_redis.js
@@ -1,18 +1,42 @@
-
-
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 var redis = require("redis")
   , subscriber = redis.createClient()
   , publisher  = redis.createClient();
- 
-subscriber.on("message", function(channel, message) {
-  console.log("Message '" + message + "' on channel '" + channel + "' arrived!");
-    if(channel == "taskA"){
-	publisher.get("A", function(err, reply){
-	    console.log(reply);
-	});
-    }
-});
 
+var sys = require("sys"),
+    net = require("net");
 
+// Redis Client
 subscriber.subscribe("taskA");
- 
+
+app.listen(5000);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  
+  subscriber.on("message", function(channel, message) {
+    console.log("Message '" + message + "' on channel '" + channel + "' arrived!");
+      if(channel == "taskA"){
+        publisher.get("A", function(err, reply){
+          socket.emit('news', { hello: reply });
+              console.log(reply);
+        });
+      }
+  });
+
+});
